897cbe6c1b87823cef9663947efabe75251284ca
Mujiwarasqf <bsundamife@icloud.com>
2025-10-29 23:57:09 +0000

    git actions update

--- infra/main.tf
+++ infra/main.tf
@@ -409,3 +409,41 @@ resource "aws_s3_object" "app" {
   content_type = "application/javascript"
   content      = file("${path.module}/ui/app.js")
 }
+
+# ---------------- GitHub Actions OIDC ----------------
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    actions = ["sts:AssumeRole"]
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:Mujiwarasqf/Task-Scheduler-Project:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "GitHubActionsRole"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,3 +3,4 @@ output "cloudfront_url"   { value = "https://${aws_cloudfront_distribution.cdn.d
 output "ui_bucket_name"   { value = aws_s3_bucket.ui.bucket }
 
 output "cloudfront_distribution_id" { value = aws_cloudfront_distribution.cdn.id }
+output "github_actions_role_arn" { value = aws_iam_role.github_actions.arn }
